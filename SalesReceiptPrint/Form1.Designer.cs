b382afd88781a1dc026447f23071a16ae38917cb
lost273 <dnk273@gmail.com>
2017-08-07 11:49:16 +0300

    Add files via upload

---
+++ SalesReceiptPrint/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace SalesReceiptPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(338, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Печать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.Document = this.printDocument1;
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(358, 10);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(763, 206);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(137, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ТОВАРНЫЙ ЧЕК №";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ИТОГО:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(64, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 363);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Печать товарного чека";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SalesReceiptPrint/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesReceiptPrint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.Columns.Add("1", "Наименование товара (услуги)");
+            dataGridView1.Columns.Add("2", "кол-во");
+            dataGridView1.Columns.Add("3", "цена(руб.коп)");
+            dataGridView1.Columns.Add("4", "сумма(руб.коп)");
+            dataGridView1.RowCount = 9;
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Height = dataGridView1.Rows.GetRowsHeight(DataGridViewElementStates.Visible)+6;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[0]).MaxInputLength = 33;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[1]).MaxInputLength = 4;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[2]).MaxInputLength = 7;
+            ((DataGridViewTextBoxColumn)dataGridView1.Columns[3]).MaxInputLength = 7;
+            
+
+
+            // Ячейки с суммой ставим readonly
+            for (int i = 0; i < 8; i++) dataGridView1.Rows[i].Cells[3].ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (printDialog1.ShowDialog() == DialogResult.OK) printDocument1.Print();
+            
+            //printPreviewDialog1.ShowDialog();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            StringFormat str = new StringFormat();
+            Font drawFont = new Font("Verdana", 10);
+            Font drawFontBig = new Font("Verdana", 14);
+            Font drawFontBoldCol = new Font("Verdana", 10, FontStyle.Bold);
+            Font drawFontBold = new Font("Verdana", 12, FontStyle.Bold | FontStyle.Italic); 
+            str.Alignment = StringAlignment.Near;
+            str.LineAlignment = StringAlignment.Center;
+            str.Trimming = StringTrimming.EllipsisCharacter;
+
+            int contWidth = 430; // ширина ячейки наименования
+            int width2 = 65; // ширина 2й ячейки
+            int width3 = 130; // ширина 3й ячейки
+            int width4 = 135; // ширина 4й ячейки
+            int widthСoordinate = 15; // координата ширины
+
+            int height = 25; // высота строки
+            int heightСoordinate = 15; // координата высоты
+
+
+            // Рисуем ООО "Сантиум", УНП 290719859, г.Брест, ул.Кирова, д.50
+            e.Graphics.DrawString("ООО \"Сантиум\", УНП 290719859, г.Брест, ул.Кирова, д.50", drawFontBig, Brushes.Black, widthСoordinate, heightСoordinate);
+            heightСoordinate = 45;
+
+            // Чек и номер
+            e.Graphics.DrawString("ТОВАРНЫЙ ЧЕК № " + textBox1.Text + " от " + dateTimePicker1.Text
+                , drawFontBold, Brushes.Black, widthСoordinate, heightСoordinate);
+            heightСoordinate = 75;
+
+            // Рисуем названия колонок
+            e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, contWidth, height);
+            e.Graphics.DrawString("Наименование товара (услуги)", drawFontBoldCol, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+            widthСoordinate += contWidth;
+            e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width2, height);
+            e.Graphics.DrawString("кол-во", drawFontBoldCol, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+            widthСoordinate += width2;
+            e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width3, height);
+            e.Graphics.DrawString("цена(руб.коп)", drawFontBoldCol, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+            widthСoordinate += width3;
+            e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width4, height);
+            e.Graphics.DrawString("сумма(руб.коп)", drawFontBoldCol, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+            heightСoordinate += height;
+
+            // Рисуем остальную таблицу
+            for (int i = 0; i < 8; i++)
+            {
+                widthСoordinate = 15;
+                e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, contWidth, height);
+                e.Graphics.DrawString((dataGridView1.Rows[i].Cells[0].Value ?? string.Empty).ToString(), drawFont, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+                widthСoordinate += contWidth;
+                e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width2, height);
+                e.Graphics.DrawString((dataGridView1.Rows[i].Cells[1].Value ?? string.Empty).ToString(), drawFont, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+                widthСoordinate += width2;
+                e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width3, height);
+                e.Graphics.DrawString((dataGridView1.Rows[i].Cells[2].Value ?? string.Empty).ToString(), drawFont, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+                widthСoordinate += width3;
+                e.Graphics.DrawRectangle(Pens.Black, widthСoordinate, heightСoordinate, width4, height);
+                e.Graphics.DrawString((dataGridView1.Rows[i].Cells[3].Value ?? string.Empty).ToString(), drawFont, Brushes.Black, widthСoordinate+3, heightСoordinate+3);
+                heightСoordinate += height;
+            }
+
+            // Рисуем Итого
+            widthСoordinate = 15;
+            e.Graphics.DrawString("ИТОГО: " + label3.Text, drawFontBold, Brushes.Black, widthСoordinate, heightСoordinate);
+            heightСoordinate += height*2;
+
+            // Рисуем Подпись продавца
+            widthСoordinate = contWidth;
+            e.Graphics.DrawString("Подпись продавца _______________________", drawFont, Brushes.Black, widthСoordinate, heightСoordinate);
+
+            
+
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            double totalSum = 0;
+            string fPartSum, sPartSum;
+
+            // Если изменения происходят в 1 или 2 столбце считаем 3 столбец
+            if ((e.ColumnIndex == 1) || (e.ColumnIndex == 2))
+            {
+                // Проверяем ячейки на отсутсвие информации
+                if ((dataGridView1.Rows[e.RowIndex].Cells[1].Value == null) || (dataGridView1.Rows[e.RowIndex].Cells[2].Value == null))
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[3].Value = null;
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[3].Value =
+                   (Convert.ToDouble(dataGridView1.Rows[e.RowIndex].Cells[1].Value) * Convert.ToDouble(dataGridView1.Rows[e.RowIndex].Cells[2].Value)).ToString("N2");
+                    
+                    // Приводим ячейки к денежному формату
+                    if (dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString().IndexOf(',') == -1)
+                        dataGridView1.Rows[e.RowIndex].Cells[2].Value = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString() + ",00";
+                    if (dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString().IndexOf(',') == -1)
+                        dataGridView1.Rows[e.RowIndex].Cells[3].Value = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString() + ",00";
+                }
+                
+                // Формируем строку ИТОГО
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    if (dataGridView1.Rows[i].Cells[3].Value != null) 
+                        totalSum = totalSum + Convert.ToDouble(dataGridView1.Rows[i].Cells[3].Value);
+                }
+
+                if (totalSum != 0)
+                {
+                    fPartSum = totalSum.ToString("N2");
+                    sPartSum = totalSum.ToString("N2");
+                    fPartSum = fPartSum.Remove(fPartSum.IndexOf(','));
+                    sPartSum = sPartSum.Remove(0,sPartSum.Length-2);
+                    label3.Text = totalSum.ToString("N2") + " (" + NumberInWords(Convert.ToInt32(fPartSum)) + " руб. " + sPartSum + " коп." + " )";
+                }
+                else label3.Text = "0";
+            }            
+            
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            TextBox tb = (TextBox)e.Control;
+            tb.KeyPress -= tb_KeyPress;
+            tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+        }
+
+        // Обработка нашатия клавиши в ячейке
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            string vlCell = ((TextBox)sender).Text;
+
+            // Проверяем ввод денежного числа
+            if (dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].IsInEditMode == true)
+
+                if ((e.KeyChar == '.') || (e.KeyChar == ','))
+                {
+                    e.KeyChar = ',';
+                    if (vlCell.IndexOf(",") != -1) e.Handled = true;
+
+                }
+                else
+                 if ((vlCell.Length - vlCell.IndexOf(',') > 2) && (vlCell.IndexOf(',') != -1) || ((e.KeyChar < '0') || (e.KeyChar > '9')))
+                {
+                    if (e.KeyChar == Convert.ToChar(Keys.Back)) return;
+
+                    e.Handled = true;
+                };
+
+        }
+
+        // Сумма прописью
+        string NumberInWords(int number)
+        {
+            int[] array_int = new int[4];
+            string[,] array_string = new string[4, 3] {{" миллиард", " миллиарда", " миллиардов"},
+                {" миллион", " миллиона", " миллионов"},
+                {" тысяча", " тысячи", " тысяч"},
+                {"", "", ""}};
+            array_int[0] = (number - (number % 1000000000)) / 1000000000;
+            array_int[1] = ((number % 1000000000) - (number % 1000000)) / 1000000;
+            array_int[2] = ((number % 1000000) - (number % 1000)) / 1000;
+            array_int[3] = number % 1000;
+            string result = "";
+            for (int i = 0; i < 4; i++)
+            {
+                if (array_int[i] != 0)
+                {
+                    if (((array_int[i] - (array_int[i] % 100)) / 100) != 0)
+                    {
+                        switch (((array_int[i] - (array_int[i] % 100)) / 100))
+                        {
+                            case 1: result += " сто"; break;
+                            case 2: result += " двести"; break;
+                            case 3: result += " триста"; break;
+                            case 4: result += " четыреста"; break;
+                            case 5: result += " пятьсот"; break;
+                            case 6: result += " шестьсот"; break;
+                            case 7: result += " семьсот"; break;
+                            case 8: result += " восемьсот"; break;
+                            case 9: result += " девятьсот"; break;
+                        }
+                    }
+                    if (((array_int[i] % 100) - ((array_int[i] % 100) % 10)) / 10 != 1)
+                    {
+                        switch (((array_int[i] % 100) - ((array_int[i] % 100) % 10)) / 10)
+                        {
+                            case 2: result += " двадцать"; break;
+                            case 3: result += " тридцать"; break;
+                            case 4: result += " сорок"; break;
+                            case 5: result += " пятьдесят"; break;
+                            case 6: result += " шестьдесят"; break;
+                            case 7: result += " семьдесят"; break;
+                            case 8: result += " восемьдесят"; break;
+                            case 9: result += " девяносто"; break;
+                        }
+                    }
+                    if (array_int[i] % 100 >= 10 && array_int[i] % 100 <= 19)
+                    {
+                        switch (array_int[i] % 100)
+                        {
+                            case 10: result += " десять"; break;
+                            case 11: result += " одиннадцать"; break;
+                            case 12: result += " двенадцать"; break;
+                            case 13: result += " тринадцать"; break;
+                            case 14: result += " четырнадцать"; break;
+                            case 15: result += " пятнадцать"; break;
+                            case 16: result += " шестнадцать"; break;
+                            case 17: result += " семнадцать"; break;
+                            case 18: result += " восемннадцать"; break;
+                            case 19: result += " девятнадцать"; break;
+                        }
+                    } else
+                    switch (array_int[i] % 10)
+                    {
+                        case 1: if (i == 2) result += " одна"; else result += " один"; break;
+                        case 2: if (i == 2) result += " две"; else result += " два"; break;
+                        case 3: result += " три"; break;
+                        case 4: result += " четыре"; break;
+                        case 5: result += " пять"; break;
+                        case 6: result += " шесть"; break;
+                        case 7: result += " семь"; break;
+                        case 8: result += " восемь"; break;
+                        case 9: result += " девять"; break;
+                    } 
+                }
+                if (array_int[i] % 100 >= 10 && array_int[i] % 100 <= 19) result += " " + array_string[i, 2] + " ";
+
+                else switch (array_int[i] % 10)
+                    {
+                        case 1: result += " " + array_string[i, 0] + " "; break;
+                        case 2:
+                        case 3:
+                        case 4: result += " " + array_string[i, 1] + " "; break;
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                        case 9: result += " " + array_string[i, 2] + " "; break;
+                    }
+            }
+            return result;
+        }
+    }
+}
